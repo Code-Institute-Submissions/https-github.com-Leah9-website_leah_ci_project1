3da2dadff107beba692e3d2590afd6f9e46e1044
Leah C <101465290+Leah9@users.noreply.github.com>
2022-07-27 11:58:48 +0100

    add recepcha code cannot test until live

--- mail-this-vith-recap.php
+++ mail-this-vith-recap.php
@@ -1,9 +1,8 @@
 <?php
-                //if (isset($_POST["SubmitBtn"])) {
-                  // 
+                  
     
-                  $to = "info@leah.wales";
-                  $subject = "Contact mail";
+                  $to = $_POST["info@leah.wales"];
+                  $subject = $_POST["Contact mail"];
                   $from = $_POST["email"];
                   $msg = $_POST["msg"];
                   $headers = "From: $from";
@@ -12,24 +11,24 @@
                   // Storing google recaptcha response
                   // in $recaptcha variable
                   
-                  //$recaptcha = $_POST['g-recaptcha-response'];
+                  $recaptcha = $_POST['g-recaptcha-response'];
                   
                   //echo "$recaptcha";
     
                   // Put secret key here, which we get
                   // from google console
 
-                  //$secret_key = '6Lfv1DYgAAAAAHyL6oRhlRyR5RAoWVk-GmVeilG0';
+                  $secret_key = '6Lfv1DYgAAAAAHyL6oRhlRyR5RAoWVk-GmVeilG0';
     
                   // Hitting request to the URL, Google will
                   // respond with success or error scenario
-                  //$url = 'https://www.google.com/recaptcha/api/siteverify?secret='
-                  //  . $secret_key . '&response=' . $recaptcha; //
+                  $url = 'https://www.google.com/recaptcha/api/siteverify?secret='
+                    . $secret_key . '&response=' . $recaptcha; //
     
                   // Making request to verify captcha
-                  //$response = file_get_contents($url); //file_get_contents is disabled on the server alternate function used.
-                  //$response = url_get_contents($url);
-                  //echo "$response";
+                  $response = file_get_contents($url); //file_get_contents is disabled on the server alternate function used.
+                  $response = url_get_contents($url);
+                  echo "$response";
     
                   // Response return by google is in
                   // JSON format, so we have to parse
@@ -37,11 +36,11 @@
                   $response = json_decode($response);
     
                   // Checking, if response is true or not
-                  //if ($response->success == true) {
-                  //  echo '<script>alert("Google reCAPTACHA verified, email sent!")</script>';
+                  if ($response->success == true) {
+                    echo '<script>alert("Google reCAPTACHA verified, email sent!")</script>';
                     mail($to, $subject, $msg, $headers);
                     echo "Email successfully sent.";
-                  //} else {
-                  //  echo '<script>alert("Error in Google reCAPTACHA")</script>';
-                  //}
-                //}?>
+                  } else {
+                    echo '<script>alert("Error in Google reCAPTACHA")</script>';
+                  }
+                ?>
